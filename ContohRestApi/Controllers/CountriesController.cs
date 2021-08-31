23b049538953d7f5f22bf301d3f6ad95aa5dd2bf
Yudha Patria <yudhapatria96@gmail.com>
2021-08-31 16:57:16 +0700

    first

---
+++ ContohRestApi/Controllers/CountriesController.cs
@@ -0,0 +1,45 @@
+﻿using ContohRestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContohRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private static List<Country> countries = new List<Country>()
+        { 
+            new Country(){ Id = 0, Name = "Indonesia", Language = "Indonesia"},
+            new Country(){ Id = 1, Name = "England", Language = "Inggris"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Country> GET()
+        {
+            return countries;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Country country)
+        {
+            countries.Add(country);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Country country)
+        {
+            countries[id] = country;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            countries.RemoveAt(id);
+        }
+    }
+}
